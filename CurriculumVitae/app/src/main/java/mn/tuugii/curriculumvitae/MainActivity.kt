044244b76271a0a38b3b50b89fe088ea3c16baef
gtuugii <tuugii83@gmail.com>
2019-12-08 23:41:52 -0600

    developed GSON - Save, Load actions

--- CurriculumVitae/app/src/main/java/mn/tuugii/curriculumvitae/MainActivity.kt
+++ CurriculumVitae/app/src/main/java/mn/tuugii/curriculumvitae/MainActivity.kt
@@ -1,11 +1,15 @@
 package mn.tuugii.curriculumvitae
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 import android.widget.RelativeLayout
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +17,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if ((ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED))
+        {
+            //write.isEnabled = false
+            ActivityCompat.requestPermissions(this, arrayOf<String>(Manifest.permission.WRITE_EXTERNAL_STORAGE), 0)
+        }
+    }
+
+    // Check permission of the camera Intent & External Storage
+    override fun onRequestPermissionsResult(requestCode:Int, permissions:Array<String>, grantResults:IntArray) {
+        if (requestCode == 0)
+        {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                //write.isEnabled = true
+            }
+        }
     }
 
     class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
