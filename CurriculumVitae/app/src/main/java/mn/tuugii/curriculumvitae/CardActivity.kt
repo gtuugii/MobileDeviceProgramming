db1b9c56794ba86289de71261e7e3263633ba4d4
gtuugii <tuugii83@gmail.com>
2019-12-08 21:35:34 -0600

    added menus

--- CurriculumVitae/app/src/main/java/mn/tuugii/curriculumvitae/CardActivity.kt
+++ CurriculumVitae/app/src/main/java/mn/tuugii/curriculumvitae/CardActivity.kt
@@ -2,20 +2,47 @@ package mn.tuugii.curriculumvitae
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.ArrayAdapter
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_card_view.*
 import mn.tuugii.curriculumvitae.classes.*
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.content_main.*
 
 class CardActivity : AppCompatActivity() {
     val dformat: SimpleDateFormat = SimpleDateFormat("MM/dd/yyyy", Locale.US)
     var personList = ArrayList<Person>()
 
+    var listItems = ArrayList<String>()
+    var adapter2: ArrayAdapter<String>? = null
+    var undoOnClickListener: View.OnClickListener = View.OnClickListener { view ->
+        listItems.removeAt(listItems.size - 1)
+        adapter2?.notifyDataSetChanged()
+        
+        Snackbar.make(view, "Item removed", Snackbar.LENGTH_LONG)
+            .setAction("Action", null).show()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_card_view)
+
+        setSupportActionBar(toolbar)
+        adapter2 = ArrayAdapter(this, android.R.layout.simple_list_item_1,
+            listItems)
+        listView.adapter = adapter2
+
+        fab.setOnClickListener { view ->
+            addListItem()
+            Snackbar.make(view, "Item added to list", Snackbar.LENGTH_LONG)
+                .setAction("Undo", undoOnClickListener).show()
+        }
 
         init()
 
@@ -23,7 +50,11 @@ class CardActivity : AppCompatActivity() {
         var adapter = CardViewAdapter(this, personList)
         rv!!.adapter = adapter
     }
-
+    private fun addListItem() {
+        val dateformat: SimpleDateFormat = SimpleDateFormat("HH:mm:ss MM/dd/yyyy", Locale.US)
+        listItems.add(dateformat.format(Date()))
+        adapter2?.notifyDataSetChanged()
+    }
     fun init(){
 
         //Puujee
@@ -110,4 +141,19 @@ class CardActivity : AppCompatActivity() {
 
     }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 }
