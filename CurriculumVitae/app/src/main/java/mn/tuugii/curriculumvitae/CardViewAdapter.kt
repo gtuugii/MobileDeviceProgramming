a215c20a942b95b8ed21008a1e5415defacab37c
gtuugii <tuugii83@gmail.com>
2019-12-08 20:51:23 -0600

    Update CardViewAdapter.kt

--- CurriculumVitae/app/src/main/java/mn/tuugii/curriculumvitae/CardViewAdapter.kt
+++ CurriculumVitae/app/src/main/java/mn/tuugii/curriculumvitae/CardViewAdapter.kt
@@ -1,26 +1,29 @@
 package mn.tuugii.curriculumvitae
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.RelativeLayout
 import android.widget.TextView
-import androidx.recyclerview.widget.RecyclerView
-import android.content.Intent
 import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
 import mn.tuugii.curriculumvitae.classes.Person
+import java.text.SimpleDateFormat
 
 class CardViewAdapter(var context : Context,
                       val pList: ArrayList<Person>) : RecyclerView.Adapter<CardViewAdapter.MyViewHolder>(){
 
     override fun onBindViewHolder(holder: MyViewHolder, pos: Int) {
 
+        val formatter = SimpleDateFormat("dd/MM/yyyy")
+
         holder.img.setImageResource(pList[pos]?.image)
         holder.fullName.text = pList[pos]?.getFullName()
         holder.position.text = "Position: " + pList[pos]?.position.toString()
-        holder.age.text = "BirthDay: " + pList[pos]?.bDate.toString()
+        holder.age.text = "BirthDay: " + formatter.format(pList[pos]?.bDate)
 
         holder.parentlayout.setOnClickListener {
             val intt = Intent(context, DetailActivity::class.java)
