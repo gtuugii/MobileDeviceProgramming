b6e14bf23f28db973a6534ae132473df41943821
Purevdemberel <purevdemberel@gmail.com>
2019-12-08 20:22:26 -0600

    updated fragmenthome, adapter, activity_card_view

--- CurriculumVitae/app/src/main/java/mn/tuugii/curriculumvitae/MainActivity.kt
+++ CurriculumVitae/app/src/main/java/mn/tuugii/curriculumvitae/MainActivity.kt
@@ -4,12 +4,23 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
+import android.widget.RelativeLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        var mumLogo : ImageView = itemView.findViewById<ImageView>(R.id.imgLogo)
+
+
+        var parentlayout : RelativeLayout = itemView.findViewById(R.id.playout) as RelativeLayout
     }
 
     fun btnEnter(view: View){
